79e49883e52ae7b9af76e05b4cd07ac48e29b6a5
seenaomi <naomi.see@seenaomi.net>
2018-04-01 23:14:32 -0500

    Let's do this again, jacked up the migrations and yeah..

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import redirect, render
-
 from lists.models import Item
 
-# Create your views here.
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-    return render(request, 'home.html')
+        return redirect('/')
 
-def view_list(request):
     items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    return render(request, 'home.html', {'items': items})
